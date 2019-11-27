04750c5d9e13842cf3feac3cd4512f135de4e770
cjs18 <36004853+cjs18@users.noreply.github.com>
2019-11-27 18:02:22 +0100

    Adding conductor.java with corrections

--- Ficheros Fuente/Conductor.java
+++ Ficheros Fuente/Conductor.java
@@ -1,38 +1,12 @@
-package Practica;
-
-
-
-
-/**
- *
+package practica;
+/*
+ * Brief descrption 
  * @authors Carlos Desco Parada
  */
 
-/* LANGUAGE NOTES
-As we can see the variables and methods names are in Spanish so in order to make it more accesible
-but not changing the names in order of the other classes to work we will provide some translations
-so more people can understand:
-
-Conductor = Driver
-Nombre = Name
-Apellidos = Surnames
-Direccion = Adress
-Gafas = Glasses
-listaConductores = Drivers list
-Carnet (de conducir) = (Driving) license
-Anadir = Add
-Busqueda = Search
-Cargar = Load
-(Conductor) habitual = Usual (Driver)
-Actualizar = Update
-Fichero = File
-Numero (Número) = Number
-Matricula = Registration plate
-Coche = Car
-Camion = Truck
-
-*/
-
+/*
+some translations
+ */
 import java.util.ArrayList;
 import java.util.Objects;
 import java.io.*;
@@ -45,20 +19,19 @@ public class Conductor implements java.io.Serializable{
     private Carnet carnet;
     private static ArrayList <Conductor> listaConductores;
     
-    public Conductor(){super();}
-    
     public Conductor(String nombre,String apellidos,String direccion,String email,boolean gafas){
-    this.nombre = nombre;
-    this.apellidos = apellidos;
-    this.direccion = direccion;
-    this.email = email;
-    this.carnet = new Carnet(gafas);
-    anadirConductor();
+	    this.nombre = nombre;
+	    this.apellidos = apellidos;
+	    this.direccion = direccion;
+	    this.email = email;
+	    this.carnet = new Carnet(gafas);
+	    anadirConductor();
     }
 
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre){
         this.nombre = nombre;
     }
@@ -66,6 +39,7 @@ public class Conductor implements java.io.Serializable{
     public String getApellidos() {
         return apellidos;
     }
+
     public void setApellidos(String apellidos){
         this.apellidos = apellidos;
     }
@@ -76,9 +50,11 @@ public class Conductor implements java.io.Serializable{
     public void setDireccion(String direccion){
         this.direccion = direccion;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email){
         this.email = email;
     }
@@ -86,92 +62,102 @@ public class Conductor implements java.io.Serializable{
     public Carnet getCarnet() {
         return carnet;
     }
+
     public void setCarnet(Carnet carnet){
         this.carnet= carnet;
     }
+
     public static ArrayList <Conductor> getListaConductores(){
-    return listaConductores;
+    	return listaConductores;
     }
     
     public static Conductor busquedaCarnet(int ncarnet){
-    for (Conductor con: listaConductores){
-        if (ncarnet == con.getCarnet().getNumeroId())
-            return con;
-    }
-    return null;
+	    for (Conductor con: listaConductores){
+	        if (ncarnet == con.getCarnet().getNumeroId())
+	            return con;
+	    }
+	    return null;
     }
     
-    public void anadirConductor(){
-    listaConductores.add(this);
-    actualizarFichero();
+    private void anadirConductor(){
+	    listaConductores.add(this);
+	    actualizarFichero();
+    }
+
+    public boolean eliminarConductor(){
+	    if(listaConductores.remove(this)){
+	    	actualizarFichero();
+	    	return true;
+	    }
+	    return false;
     }
     
     public static Conductor busquedaMatricula(String mat){
-    ArrayList <Coche> lista1 = Coche.getListaCoches();
-    ArrayList <Camion> lista2 = Camion.getListaCamiones();
-    if(lista1.contains(Coche.busquedaMatricula(mat)))
-        return Coche.busquedaMatricula(mat).getHabitual();
-    else if(lista2.contains(Camion.busquedaMatricula(mat)))
-            return Camion.busquedaMatricula(mat).getHabitual();
-    return new Conductor();
+	    ArrayList <Coche> lista1 = Coche.getListaCoches();
+	    ArrayList <Camion> lista2 = Camion.getListaCamiones();
+	    if(lista1.contains(Coche.busquedaMatricula(mat))){
+	        return Coche.busquedaMatricula(mat).getHabitual();
+	    }
+	    else if(lista2.contains(Camion.busquedaMatricula(mat))){
+	        return Camion.busquedaMatricula(mat).getHabitual();
+	    }
+	    return new Conductor();
     }
     
     public static void cargarConductor(){
-    listaConductores = new ArrayList();
-    ArrayList<Conductor> listaaux = new ArrayList<>();
-    boolean haysig = true;
-    try{
-    FileInputStream fis = new FileInputStream("Conductores.bin");
-    ObjectInputStream input = new ObjectInputStream(fis);
-    while(haysig){
-      Conductor con = (Conductor) input.readObject();
-      if(con != null)
-         listaaux.add(con);
-      else
-         haysig = false;
-   }
-    }
-    catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            //e.printStackTrace();
-        }
-    catch(IOException | ClassNotFoundException e){
-   //System.out.println(e.printStackTrace());
-}
-    
-listaConductores.addAll(listaaux);
-
-    }
+	    listaConductores = new ArrayList();
+	    ArrayList<Conductor> listaaux = new ArrayList<>();
+	    boolean haysig = true;
+	    try{
+		    FileInputStream fis = new FileInputStream("Conductores.bin");
+		    ObjectInputStream input = new ObjectInputStream(fis);
+		    while(haysig){
+		      	Conductor con = (Conductor) input.readObject();
+		      	if(con != null){
+		        	listaaux.add(con);
+		      	}
+		      	else{
+		        	haysig = false;
+		      	}
+		    }
+	    }catch (FileNotFoundException e) {
+	           e.printStackTrace();
+	    }catch(IOException){
+	    	System.out.println(e.printStackTrace());
+	    }catch(ClassNotFoundException e){
+	    	e.printStackTrace();
+	    }
+		listaConductores.addAll(listaaux);
+	}
     
     public static void actualizarFichero(){
-     try {
-            FileOutputStream fos = new FileOutputStream("Conductores.bin");
-            try {
-                ObjectOutputStream oos = new ObjectOutputStream(fos);
+     	try{
+	        FileOutputStream fos = new FileOutputStream("Conductores.bin");
+	        try {
+	        	ObjectOutputStream oos = new ObjectOutputStream(fos);
                 for (Conductor con : listaConductores){
-                    try {
+                    try{
                         oos.writeObject(con);
                         System.out.println("saved");
-                    } catch (NotSerializableException e) {
+                    }catch (NotSerializableException e) {
                         System.out.println("Objeto no serializable");
                         e.printStackTrace();
                     }
                 }
-            } catch (IOException e) {
+            }catch (IOException e) {
                 e.printStackTrace();
-            }
-        } catch (FileNotFoundException e) {
+            }catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
-    }
-    
+        	}
+    	}
+    }    
 
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
             return true;
         }
-        if (obj == null) {
+        if (!obj) {
             return false;
         }
         if (getClass() != obj.getClass()) {
@@ -191,17 +177,9 @@ listaConductores.addAll(listaaux);
     }
 
     @Override
-    public String toString() {
-        return "Numero de Carnet:   " + Integer.toString(getCarnet().getNumeroId())+ " | Nombre:  " + getNombre() + " | Apellidos:  " + getApellidos()+ " | Direccion:  " + getDireccion() + " |  Email:   "+ getEmail();
+    public String toString(){
+        return "Numero de Carnet: " + Integer.toString(getCarnet().getNumeroId())+ " | Nombre:  " + getNombre() + " | Apellidos:  " 
+        		+ getApellidos()+ " | Direccion:  " + getDireccion() + " |  Email:   "+ getEmail();
     }
     
-    
-    
-    public boolean eliminarConductor(){
-    if(listaConductores.remove(this)){
-    actualizarFichero();
-    return true;
-            }
-    return false;
-    }
 }
